6787156bff4bf5e442e68391fa1836433cdc4d8b
almonte914 <almonte914@gmail.com>
2022-02-05 18:59:37 -0600

    Update point.js

--- point.js
+++ point.js
@@ -2,15 +2,19 @@
 
 var currentX = 100;
 var currentY = 100;
+var dx = 5;
+var dy = 5;
 
 function animate(){
     document.getElementById('ball').style.left = currentX + 'px';
     document.getElementById('ball').style.top = currentY + 'px';
-    currentX += 5;
-    currentY += 5;
-    if(currentX > 800 || currentY > 600){
-        return;
+    currentX += dx;
+    currentY += dy;
+    if((currentX > 800 || currentX < 100) || (currentY > 600 || currentY < 100)){
+        dx *= -1;
+        dy *= -1;
     }
     setTimeout ('animate()', 10);
+
 }
 window.addEventListener('load',animate);
